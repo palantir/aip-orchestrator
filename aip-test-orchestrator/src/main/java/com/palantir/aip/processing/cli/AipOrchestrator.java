921836c165d499c62278aeb31f3dc66188bf3ea3
Kevin Bi <kevinb@palantir.com>
2022-03-09 14:41:28 -0500

    Update AipOrchestrator to allow v3 version from CLI

--- aip-test-orchestrator/src/main/java/com/palantir/aip/processing/cli/AipOrchestrator.java
+++ aip-test-orchestrator/src/main/java/com/palantir/aip/processing/cli/AipOrchestrator.java
@@ -5,8 +5,10 @@
 package com.palantir.aip.processing.cli;
 
 import com.google.common.net.HostAndPort;
+import com.palantir.aip.processing.aip.AipInferenceProcessorClientV3;
 import com.palantir.aip.processing.orchestrators.V2ProcessorOrchestrator;
 import com.palantir.aip.processing.aip.AipInferenceProcessorClientV2;
+import com.palantir.aip.processing.orchestrators.V3ProcessorOrchestrator;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import java.net.URI;
@@ -45,13 +47,27 @@ public final class AipOrchestrator implements Runnable {
             defaultValue = "0.2")
     private double framesPerSecond;
 
+    @CommandLine.Option(
+            names = "--version",
+            description = "The version of processor to test. Valid options are: v2, v3video, v3imagery",
+            defaultValue = "v3video"
+    )
+    private String version;
 
 
-    public static AipInferenceProcessorClientV2 grpc(HostAndPort hostAndPort, String productName, String productVersion) {
+
+    public static AipInferenceProcessorClientV2 grpcV2(HostAndPort hostAndPort, String productName, String productVersion) {
         ManagedChannel channel = ManagedChannelBuilder.forAddress(hostAndPort.getHost(), hostAndPort.getPort())
                 .usePlaintext()
                 .build();
         return new AipInferenceProcessorClientV2(channel, productName, productVersion);
+    }
+
+    public static AipInferenceProcessorClientV3 grpcV3(HostAndPort hostAndPort, String productName, String productVersion) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(hostAndPort.getHost(), hostAndPort.getPort())
+                .usePlaintext()
+                .build();
+        return new AipInferenceProcessorClientV3(channel, productName, productVersion);
     }
 
     public static void main(String... args) {
@@ -63,8 +79,22 @@ public final class AipOrchestrator implements Runnable {
         System.out.println("Orchestrator: running");
         System.out.println("Frames per second: " + framesPerSecond);
 
+        switch(version.toLowerCase()) {
+            case "v2":
+                handleV2();
+                break;
+            case "v3video":
+                handleV3Video();
+                break;
+            case "v3imagery":
+                handleV3Imagery();
+                break;
+        }
+    }
+
+    private void handleV2() {
         System.out.println("Sending configuration request to server...");
-        AipInferenceProcessorClientV2 processor = grpc(
+        AipInferenceProcessorClientV2 processor = grpcV2(
                         HostAndPort.fromParts(uri.getHost(), uri.getPort()),
                         "AIP Orchestrator",
                         Optional.ofNullable(AipOrchestrator.class.getPackage().getImplementationVersion())
@@ -83,5 +113,51 @@ public final class AipOrchestrator implements Runnable {
         long nanosPerFrame = (long) ((1.0 / framesPerSecond) * 1_000_000_000);
         ScheduledExecutorService executor =  Executors.newScheduledThreadPool(1);
         dispatcher.sendAtFixedRate(executor, nanosPerFrame, TimeUnit.NANOSECONDS);
+    }
+
+    private void handleV3Video() {
+        System.out.println("Sending configuration request to server...");
+        AipInferenceProcessorClientV3 processor = grpcV3(
+                HostAndPort.fromParts(uri.getHost(), uri.getPort()),
+                "AIP Orchestrator",
+                Optional.ofNullable(AipOrchestrator.class.getPackage().getImplementationVersion())
+                        .orElse("0.0.0"));
+
+        try {
+            processor.configure();
+        } catch (RuntimeException e) {
+            System.out.println("Error when initializing processor" + e.toString());
+            throw e;
+        }
+        System.out.println("Processor configured. Getting ready to send inference requests.");
+
+        V3ProcessorOrchestrator dispatcher = new V3ProcessorOrchestrator(sharedImagesDir, processor);
+
+        long nanosPerFrame = (long) ((1.0 / framesPerSecond) * 1_000_000_000);
+        ScheduledExecutorService executor =  Executors.newScheduledThreadPool(1);
+        dispatcher.sendVideoAtFixedRate(executor, nanosPerFrame, TimeUnit.NANOSECONDS);
+    }
+
+    private void handleV3Imagery() {
+        System.out.println("Sending configuration request to server...");
+        AipInferenceProcessorClientV3 processor = grpcV3(
+                HostAndPort.fromParts(uri.getHost(), uri.getPort()),
+                "AIP Orchestrator",
+                Optional.ofNullable(AipOrchestrator.class.getPackage().getImplementationVersion())
+                        .orElse("0.0.0"));
+
+        try {
+            processor.configure();
+        } catch (RuntimeException e) {
+            System.out.println("Error when initializing processor" + e);
+            throw e;
+        }
+        System.out.println("Processor configured. Getting ready to send inference requests.");
+
+        V3ProcessorOrchestrator dispatcher = new V3ProcessorOrchestrator(sharedImagesDir, processor);
+
+        long nanosPerFrame = (long) ((1.0 / framesPerSecond) * 1_000_000_000);
+        ScheduledExecutorService executor =  Executors.newScheduledThreadPool(1);
+        dispatcher.sendImageryAtFixedRate(executor, nanosPerFrame, TimeUnit.NANOSECONDS);
     }
 }
